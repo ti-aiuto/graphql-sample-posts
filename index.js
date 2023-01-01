52c6dd397052177a96eaf9ec5e6b6887df22570f
ti-aiuto <ti.aiuto@outlook.jp>
2023-01-01 17:12:48 +0900

    :pencil2: かえた

--- index.js
+++ index.js
@@ -1,40 +1,100 @@
 const express = require('express')
 const express_graphql = require('express-graphql').graphqlHTTP
-const { buildSchema } = require('graphql')
-
-const schema = buildSchema(`
-  type Query {
-    post(id: Int!): Post
-    posts: [Post]
-  }
-  
-  type Post {
-    id: Int
-    title: String
-    author: String
-    content: String
-    message: Message
-  }
-`)
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLNonNull,
+  GraphQLList
+} = require('graphql')
 
 const dummyPosts = [
-  { id: 1, title: 'titleXXXX', author: 'yamada', content: 'abcdefg' },
-  { id: 2, title: 'titleYYYY', author: 'suzuki', content: '1234567' },
-  { id: 3, title: 'titleZZZZ', author: 'tanaka', content: 'ABCDEFG' }
+  { id: 1, title: 'titleXXXX', content: 'abcdefg' },
+  { id: 2, title: 'titleYYYY', content: '1234567' },
+  { id: 3, title: 'titleZZZZ', content: 'ABCDEFG' }
 ]
-const root = {
-  post: args => {
-    const id = args.id
-    return dummyPosts.filter(post => post.id == id)[0]
-  },
-  posts: () => dummyPosts
-}
+
+const dummyComments = [
+  { id: 1, postId: 1, author: 'authorXXXX', body: 'aaaaaaaaaaaa' },
+  { id: 2, postId: 2, author: 'authorYYYY', body: 'bbbbbbbbbbbb' },
+  { id: 3, postId: 1, author: 'authorZZZZ', body: 'cccccccccccc' },
+  { id: 4, postId: 3, author: 'authorYYYY', body: 'dddddddddddd' }
+]
+
+const CommentType = new GraphQLObjectType({
+  name: 'CommentType',
+  fields: {
+    id: {
+      type: GraphQLInt
+    },
+    postId: {
+      type: GraphQLInt
+    },
+    author: {
+      type: GraphQLString
+    },
+    body: {
+      type: GraphQLString
+    }
+  }
+})
+
+const PostType = new GraphQLObjectType({
+  name: 'PostType',
+  fields: {
+    id: {
+      type: GraphQLInt
+    },
+    title: {
+      type: GraphQLString
+    },
+    content: {
+      type: GraphQLString
+    },
+    comments: {
+      type: new GraphQLList(CommentType),
+      resolve(parent, args) {
+        const postId = parent.id
+        return dummyComments.filter(comment => comment.postId == postId)
+      }
+    }
+  }
+})
+
+const RootType = new GraphQLObjectType({
+  name: 'RootType',
+  fields: {
+    posts: {
+      type: new GraphQLList(PostType),
+      resolve() {
+        return dummyPosts
+      }
+    },
+    post: {
+      type: PostType,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLInt)
+        }
+      },
+      resolve(parent, args) {
+        const id = args.id
+        return dummyPosts.filter(post => post.id == id)[0]
+      }
+    }
+  }
+})
+
+const schema = new GraphQLSchema({
+  query: RootType
+})
 
 const app = express()
 app.use('/graphql', express_graphql({
   schema: schema,
-  rootValue: root,
   graphiql: true
 }))
 
 app.listen(5000, () => console.log('Example app listening on port 5000!'))
+
