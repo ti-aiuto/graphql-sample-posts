08509a87583aa9bbad5e4f9d2445a3b04c1fa451
ti-aiuto <ti.aiuto@outlook.jp>
2023-01-01 21:42:18 +0900

    :heavy_plus_sign: 追加

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,20 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./schema";
+import { typeDefs } from "./schema.js";
+import mysql from "mysql2";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const connection = mysql.createConnection({
+  host: process.env.SAMPLE_POSTS_DATABASE_HOST,
+  user: process.env.SAMPLE_POSTS_DATABASE_USERNAME,
+  password: process.env.SAMPLE_POSTS_DATABASE_USERNAME,
+  database: process.env.SAMPLE_POSTS_DATABASE_DATABASE,
+});
 
 const resolvers = {
   Query: {},
+  Mutation: {},
 };
 
 const server = new ApolloServer({
