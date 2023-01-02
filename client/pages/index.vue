bdc5cd8b5046e1ef7ff7c95aca3d1432351912bf
ti-aiuto <ti.aiuto@outlook.jp>
2023-01-03 00:34:46 +0900

    :pencil2: 調整

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,60 +1,62 @@
 <template>
   <div class="container">
     <v-app>
-      <v-dialog v-model="dialog" max-width="600px">
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn color="primary" dark v-bind="attrs" v-on="on">
-            新規投稿
-          </v-btn>
-        </template>
-        <form @submit.prevent="submitPostForm">
-          <v-card>
-            <v-card-title>
-              <span class="text-h5">新規投稿</span>
-            </v-card-title>
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col cols="12">
-                    <v-textarea
-                      label="本文を入力..."
-                      required
-                      v-model="editingContent"
-                    />
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text type="submit">投稿</v-btn>
-            </v-card-actions>
-          </v-card>
-        </form>
-      </v-dialog>
-
-      <div>
-        <v-timeline dense>
-          <v-timeline-item
-            v-for="post in posts"
-            :key="post.id"
-            small
-            color="teal lighten-3"
-          >
+      <v-container>
+        <v-dialog v-model="dialog" max-width="500px">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn color="primary" dark v-bind="attrs" v-on="on" width="100%">
+              新規投稿
+            </v-btn>
+          </template>
+          <form @submit.prevent="submitPostForm">
             <v-card>
+              <v-card-title>
+                <span class="text-h5">新規投稿</span>
+              </v-card-title>
               <v-card-text>
-                <div
-                  class="text--primary mb-2"
-                  v-text="post.content"
-                  style="white-space: pre"
-                />
-
-                <div v-text="post.createdAt" />
+                <v-container>
+                  <v-row>
+                    <v-col cols="12">
+                      <v-textarea
+                        label="本文を入力..."
+                        required
+                        v-model="editingContent"
+                      />
+                    </v-col>
+                  </v-row>
+                </v-container>
               </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="blue darken-1" text type="submit">投稿</v-btn>
+              </v-card-actions>
             </v-card>
-          </v-timeline-item>
-        </v-timeline>
-      </div>
+          </form>
+        </v-dialog>
+
+        <div>
+          <v-timeline dense>
+            <v-timeline-item
+              v-for="post in posts"
+              :key="post.id"
+              small
+              color="teal lighten-3"
+            >
+              <v-card>
+                <v-card-text>
+                  <div
+                    class="text--primary mb-2"
+                    v-text="post.content"
+                    style="white-space: pre"
+                  />
+
+                  <div v-text="post.createdAt" />
+                </v-card-text>
+              </v-card>
+            </v-timeline-item>
+          </v-timeline>
+        </div>
+      </v-container>
     </v-app>
   </div>
 </template>
