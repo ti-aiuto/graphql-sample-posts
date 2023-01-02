fec1dfc99c2f378fba06c6882b66712df65468b6
ti-aiuto <ti.aiuto@outlook.jp>
2023-01-03 00:18:39 +0900

    :heavy_plus_sign: ボタン設置

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,6 +1,38 @@
 <template>
   <div class="container">
     <v-app>
+      <v-dialog v-model="dialog" persistent max-width="600px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn color="primary" dark v-bind="attrs" v-on="on">
+            新規投稿
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-title>
+            <span class="text-h5">新規投稿</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="12">
+                  <v-textarea label="本文を入力..." required />
+                </v-col>
+              </v-row>
+            </v-container>
+            <small>*indicates required field</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="dialog = false">
+              Close
+            </v-btn>
+            <v-btn color="blue darken-1" text @click="dialog = false">
+              Save
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+
       <div>
         <v-timeline dense>
           <v-timeline-item
@@ -27,6 +59,9 @@
 import listQuery from "../queries/list.gql";
 
 export default {
+  data() {
+    return { dialog: false };
+  },
   apollo: {
     posts: {
       prefetch: true,
